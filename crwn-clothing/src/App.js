526a0d04321099a912fa0d88412cf56f4839601f
spgitmonish <monishsp@gmail.com>
2020-05-14 13:11:58 -0700

    Components and styling

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HomePage from './page/homepage/homepage.component'
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
