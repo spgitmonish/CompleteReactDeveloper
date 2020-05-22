ebdb915b8ca6d5cb6620bbf0e1c0f3ab2508cee4
spgitmonish <monishsp@gmail.com>
2020-05-22 16:25:55 -0700

    Header component with logo, shop and contact link

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -3,20 +3,15 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './page/homepage/homepage.component'
 import ShopPage from './page/shop/shop.component'
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS Page</h1>
-  </div>
-);
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route exact path='/shop' component={ShopPage}></Route>
-        <Route exact path='/shop/hats' component={HatsPage}></Route>
       </Switch>
     </div>
   );
