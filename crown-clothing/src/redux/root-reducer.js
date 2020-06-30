52946038d8f53203c23779c2d08048e4a27b8897
spgitmonish <monishsp@gmail.com>
2020-06-29 20:26:58 -0700

    Add redux persist for local storage

--- crown-clothing/src/redux/root-reducer.js
+++ crown-clothing/src/redux/root-reducer.js
@@ -1,9 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+// Local storage as default storage
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({ 
+// Define a new persist config
+const persistConfig = {
+  key: 'root',
+  storage,
+  whiteList: ['cart'] // Reducers to persist
+}
+
+const rootReducer = combineReducers({ 
   user: userReducer,
   cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
